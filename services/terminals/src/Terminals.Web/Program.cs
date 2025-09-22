6345eaf80f98266a132153d8507424e193f25b00
Daniil Molotkov <molotkov.58@ya.ru>
2025-09-23 00:32:24 +0700

    down to net 8

--- services/terminals/src/Terminals.Web/Program.cs
+++ services/terminals/src/Terminals.Web/Program.cs
@@ -9,15 +9,8 @@ namespace Terminals.Web
 
 
             builder.Services.AddControllers();
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
 
             app.UseAuthorization();
 
